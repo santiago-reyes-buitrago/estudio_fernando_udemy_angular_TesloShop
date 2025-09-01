2f416ad11a64c3069234e551db52fa846e9bb731
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-09-01 08:46:49 -0500

    Terminada seccion 18 - se salta la seccion 19 y se empieza la seccion 20

--- src/app/products/components/carrousel/carrousel.ts
+++ src/app/products/components/carrousel/carrousel.ts
@@ -1,5 +1,12 @@
-import {Component, input} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, input, viewChild} from '@angular/core';
 import {CarrouselItem} from '@products/components/carrousel-item/carrousel-item';
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import {Navigation, Pagination} from 'swiper/modules';
 
 @Component({
   selector: 'product-list-carrousel',
@@ -9,7 +16,38 @@ import {CarrouselItem} from '@products/components/carrousel-item/carrousel-item'
   templateUrl: './carrousel.html',
   styleUrl: './carrousel.css'
 })
-export class Carrousel {
+export class Carrousel implements AfterViewInit {
+  swiperContainer = viewChild.required<ElementRef>('swiperContainer')
   images = input.required<string[]>()
-  vertical = input<boolean>(false)
+  direction = input<"horizontal" | "vertical">('horizontal')
+
+  ngAfterViewInit(): void {
+    const element = this.swiperContainer().nativeElement;
+    if (!element) return;
+    const swiper = new Swiper(element, {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 1,
+
+      modules: [Navigation,Pagination],
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }
+
 }
--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {rxResource, toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs';
+import {ProductService} from '@products/services/product.service';
+import {ListCard} from '@products/components/list-card/list-card';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [
+    ListCard
+  ],
   templateUrl: './gender-page.html',
   styleUrl: './gender-page.css'
 })
 export class GenderPage {
+  route = inject(ActivatedRoute);
+  productService = inject(ProductService);
+  gender = toSignal(this.route.params.pipe(map(({gender})=> gender)  ))
 
+  genderResource = rxResource({
+    params: () => ({
+      gender: this.gender()
+    }),
+    stream: ({params})=> {
+      return this.productService.getProducts({
+        limit: 10,
+        gender: params.gender
+      })
+    }
+  })
 }
